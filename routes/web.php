c09bf15c8e4eec1c33efbae6ee80bcfafd1e3723
Inna Tarasyan <innatarasyanmail@gmail.com>
2025-10-14 15:07:30 +0400

    Added tales pages

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\TaleController::class, 'index'])->name('home');
 
 use App\Http\Controllers\TaleController;
 
